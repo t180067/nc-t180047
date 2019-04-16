9139f2ce7224b985e990fb5225e42f56a691efca
t180047 <49540108+t180047@users.noreply.github.com>
2019-04-16 10:43:25 +0900

    Update kadai1-3.c

--- kadai1-3.c
+++ kadai1-3.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(void){
+	double x,y,z;
+	double max;
+	int s,t;
+	int a=10;
+	
+	for(s=-a;s<=a;s++){
+		x=(double)s/(double)a;
+		for(t=-a;t<=a;t++){	
+			y=(double)t/(double)a;
+			z=2*(x+y)/(x*x+y*y+1);
+			if(s==-a && t==-a)
+				max=z;
+			if(max<z)
+				max=z;
+		}
+	}
+	printf("max:%f\n",max);
+	return 0;
+}
