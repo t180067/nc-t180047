ce7da49da9f7f5bf610394c5180a961a112810b6
t180047 <49540108+t180047@users.noreply.github.com>
2019-04-22 00:42:11 +0900

    Update kadai2-4.c

--- kadai2-4.c
+++ kadai2-4.c
@@ -5,11 +5,9 @@ int main ()
   int i = 1;
  
   /* 以下に適当な変更を加えよ */
-  
-  while (1) {
-    i = i*2;
-    printf("%d\n", i);
-    if ( i < 0 ) break;
+  while(1){
+    i++;
+    printf("%d\n",i);
+    if(i<0)break;
   }
-
 }
