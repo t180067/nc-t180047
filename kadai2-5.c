bbe64add276d8fca5812d4c1a25367e214e8fc83
t180047 <49540108+t180047@users.noreply.github.com>
2019-04-22 00:27:07 +0900

    Update kadai2-5.c

--- kadai2-5.c
+++ kadai2-5.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main ()
-{
-  long long i = 1;
-
-  while (1) {
-    i = i*2;
-    printf("%lld\n", i);
-    if ( i < 0 ) break;
-  }
-
+int main(){
+    long long i=1;
+    while(1){
+        i=i*2;
+        if(i<0)break;
+    }
+    printf("%lld\n",i-1);
 }
