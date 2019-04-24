9240b86e0e13ab57031c53da9249b5b8b91a5a70
t180047 <49540108+t180047@users.noreply.github.com>
2019-04-25 00:25:59 +0900

    Update kadai3-4.c

--- kadai3-4.c
+++ kadai3-4.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
 
+int main(){
+    int t;
+    double x;
+    int y[10]={};
+
+    while(1){
+        printf("x? ");
+        scanf("%lf",&x);
+        if(x<=1.0) break;
+    }
+
+    for(t=0;t<10;t++){
+        x=x*2.0;
+        if(x>=1.0){
+            y[t]=(int)x;
+            x=x-1.0;
+        }
+        else
+            y[t]=(int)x;
+    }
+    printf("0.");
+    for(t=0;t<10;t++){
+        printf("%d",y[t]);
+    }
+    printf("\n");
+}
