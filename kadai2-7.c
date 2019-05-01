86ae0df2811c6da1636869a99bd121801c27a3c3
t180047 <49540108+t180047@users.noreply.github.com>
2019-05-01 14:13:01 +0900

    Update kadai2-7.c

--- kadai2-7.c
+++ kadai2-7.c
@@ -1,27 +1,14 @@
-#include <stdio.h>
-
-long long fac (int n)
+#include<stdio.h>
+	
+ long long fac (int n)
 {
   return (n == 0) ? 1 : n*fac(n-1);
 }
 
-long long binomial (int n, int k)
-{ 
-  return fac(n)/(fac(k)*fac(n-k));
-
-}
-int main ()
+ int main ()
 {
   int i;
-
-  for (i = 0; i <= 12; i++) {
-    printf("12C%d = %lld\n",i,binomial(12,i));
-  }
-
-  printf("\n");
-
-  for (i = 0; i <= 13; i++) {
-    printf("13C%d = %lld\n",i,binomial(13,i));
-  }
-
+  for (i = 1; i <= 16; i++) 
+    printf("%2d! = %lld\n", i, fac(i));
+  
 }
