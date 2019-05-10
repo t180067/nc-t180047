6bcc26558536537904f8185fb44805468f3a9795
t180047 <49540108+t180047@users.noreply.github.com>
2019-05-10 11:49:52 +0900

    Update kadai4-3.c

--- kadai4-3.c
+++ kadai4-3.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main(){
+    int a=1;
+    int n,i;
+
+    printf("N?");
+    scanf("%d",&n);
+
+    for(i=0;i<=n;i++){
+        printf("a%d = %d\n",i,a);
+        a = 2*a+1;
+    }
+}
