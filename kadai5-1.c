26574e3c3a6b275b76e219821fffe02d3e8203a5
t180047 <49540108+t180047@users.noreply.github.com>
2019-05-10 12:23:21 +0900

    Update kadai5-1.c

--- kadai5-1.c
+++ kadai5-1.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#define Pi 3.14159265
 
+int main(){
+	int n=0;
+	double sum=0,a=2.0;
+	
+	while(n<24){
+		sum = sum + a;
+		printf("n = %d :Pi = %.8f,error : %e\n",n,sum,Pi-sum);
+		n++;
+		a = a*n/(2*n+1);
+	}
+}
