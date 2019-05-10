7ed950cebb2b38b9b99ba829b2649e0890bf6c10
t180047 <49540108+t180047@users.noreply.github.com>
2019-05-10 11:48:51 +0900

    Update kadai4-1.c

--- kadai4-1.c
+++ kadai4-1.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main(){
+    int a=0;
+    int n,i;
+
+    printf("N?");
+    scanf("%d",&n);
+
+    for(i=0;i<=n;i++){
+        printf("a%d = %d\n",i,a);
+        a += 2;
+    }
+}
